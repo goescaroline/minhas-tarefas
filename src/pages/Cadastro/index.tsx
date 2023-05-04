390649112fa59f349611b5542f7528da3dbca191
goescaroline <goes_caroline@hotmail.com>
2023-05-04 15:34:54 -0300

    criando aplicaçao com redux

---
+++ src/pages/Cadastro/index.tsx
@@ -0,0 +1,10 @@
+import BarraLateral from '../../container/BarraLateral'
+import Formulario from '../../container/Formulario'
+
+const Cadastro = () => (
+  <>
+    <BarraLateral mostrarFiltros={false} />
+    <Formulario />
+  </>
+)
+export default Cadastro
